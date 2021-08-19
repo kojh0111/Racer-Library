418e5d14dfa72f0e6b7d38df3eef00f5ccdb5e50
jh <kojh0111@naver.com>
2021-08-19 16:15:58 +0900

    Library Automatic Return Book

--- library/views/main_view.py
+++ library/views/main_view.py
@@ -1,4 +1,5 @@
-from models import libraryBook
+from datetime import date
+from models import libraryBook, rentalBook, db
 from flask import Blueprint, redirect, render_template, request, url_for
 from math import ceil
 
@@ -17,6 +18,19 @@ def home():
     offset = limit - page_size
     page_count = ceil(libraryBook.query.count() / page_size)
     books = libraryBook.query.order_by(libraryBook.id.asc())[offset:limit]
+    for book in books:
+        libBook = libraryBook.query.filter(libraryBook.id == book.id).first()
+        borBook = (
+            rentalBook.query.filter(rentalBook.book_id == book.id)
+            .order_by(rentalBook.rental_date.desc())
+            .first()
+        )
+        try:
+            if borBook.return_date < date.today():
+                libBook.rented = False
+                db.session.commit()
+        except AttributeError:
+            continue
     page_range = range(1, page_count + 1)
     if page < 1 or page > page_count:
         return redirect(url_for("index"))
