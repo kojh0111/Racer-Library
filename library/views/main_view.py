667a8a60886f8deb7dc9b1e2a02838e85c0a18c6
jh <kojh0111@naver.com>
2021-08-19 10:33:06 +0900

    Pagination

--- library/views/main_view.py
+++ library/views/main_view.py
@@ -1,10 +1,24 @@
 from models import libraryBook
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from math import ceil
 
 bp = Blueprint("home", __name__, url_prefix="/")
 
 
 @bp.route("/")
 def home():
-    books = libraryBook.query.order_by(libraryBook.id.asc())
-    return render_template("index.html", books=books)
+    page = request.args.get("page", 1)
+    page = int(page)
+    page_size = 8
+    limit = page * page_size
+    offset = limit - page_size
+    page_count = ceil(libraryBook.query.count() / page_size)
+    books = libraryBook.query.order_by(libraryBook.id.asc())[offset:limit]
+    page_range = range(1, page_count + 1)
+    return render_template(
+        "index.html",
+        page=page,
+        books=books,
+        page_count=page_count,
+        page_range=page_range,
+    )
